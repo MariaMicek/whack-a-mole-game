bf7e915b69cfee13256435d38174737e79b5cf5c
Maria Micek <mmicek16@wp.pl>
2019-04-13 21:42:10 +0200

    startGame, endGame, showMole

--- src/Game.js
+++ src/Game.js
@@ -3,52 +3,96 @@ import Whole from './Whole'
 import Button from '@material-ui/core/Button'
 
 class Game extends Component {
-  state = {
-    wholes: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ],
-    isGameStarted: false,
-    score: 0
-  }
+	state = {
+		wholes: [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9]
+		],
+		isGameStarted: false,
+		randomWhole: null,
+		score: 0
+	}
 
-  render() {
-    return (
-      <div>
-        <h2>WHACK A MOLE</h2>
-        <div
-          className={'board'}
-        >
-          {
-            this.state.wholes.map(
-              (row, index) => (
-                <div
-                  key={'row' + index}
-                  className={'board-row'}
-                >
-                  {
-                    row.map(
-                      (whole, index) => (
-                        <Whole
-                          key={index}
-                        />
-                      )
-                    )
-                  }
-                </div>
-              )
-            )
-          }
-        </div>
-        <Button
-		  variant={'contained'}
-        >
-          START
+	randomWhole = () => {
+		const randomRowIndex = Math.floor(Math.random() * this.state.wholes.length)
+		const randomRowArray = this.state.wholes[randomRowIndex]
+		const randomWholeIndex = Math.floor(Math.random() * randomRowArray.length)
+		const randomWhole = this.state.wholes[randomRowIndex][randomWholeIndex]
+		if (randomWhole === this.state.randomWhole) {
+			return this.randomWhole()
+		}
+		this.setState({ randomWhole: randomWhole })
+	}
+
+	startGame = () => {
+		if (this.state.isGameStarted) return
+		this.setState({ isGameStarted: true })
+		this.showMole()
+	}
+
+	showMole = () => {
+		const showMole = setInterval(
+			this.randomWhole,
+			500
+		)
+		this.endGame(showMole)
+	}
+
+	endGame = (showMole) => {
+		setTimeout(
+			() => {
+				clearInterval(showMole)
+				this.setState({ isGameStarted: false, randomWhole: null })
+			},
+			5000
+		)
+	}
+
+	render() {
+		console.log(this.state.isGameStarted)
+		return (
+			<div>
+				<h2>WHACK A MOLE</h2>
+				<div
+					className={'board'}
+				>
+					{
+						this.state.wholes.map(
+							(row, rowIndex, array) => (
+								<div
+									key={'row' + rowIndex}
+									className={'board-row'}
+								>
+									{
+										row.map(
+											(whole, wholeIndex) => (
+												<Whole
+													key={wholeIndex}
+													className={
+														this.state.randomWhole === array[rowIndex][wholeIndex] ?
+															'whole active'
+															:
+															'whole'
+													}
+												/>
+											)
+										)
+									}
+								</div>
+							)
+						)
+					}
+				</div>
+				<Button
+					variant={'contained'}
+					onClick={this.startGame}
+				>
+					START
         </Button>
-      </div>
-    )
-  }
+			</div>
+		)
+	}
 }
 
 export default Game
--- src/Whole.js
+++ src/Whole.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Whole = (props) => {
     return (
         <div
-            className={'whole'}
+           className={props.className}
         >
 
         </div>
